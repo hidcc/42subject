b0bd5a8ddd2d1f232bd5db9218f04dd9e223c331
hakuta <hidenoriaku@gmail.com>
2026-04-29 09:30:48 +0900

    fix

--- ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: hakuta <hakuta@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/28 17:54:41 by hakuta            #+#    #+#             */
-/*   Updated: 2026/04/28 17:54:42 by hakuta           ###   ########.fr       */
+/*   Updated: 2026/04/29 09:14:44 by hakuta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,28 @@
 
 int	ft_putchar(char c)
 {
-	if (write(1, &c, 1) < 0)
+	ssize_t	ret;
+
+	ret = write(1, &c, 1);
+	if (ret < 0)
 		return (-1);
 	return (1);
 }
 
 int	ft_putstr(char *s)
 {
-	int	count;
+	size_t	count;
+	ssize_t	ret;
 
 	if (!s)
 		return (ft_putstr("(null)"));
 	count = 0;
 	while (s[count])
 		count++;
-	if (count > 0 && write(1, s, count) < 0)
+	if (count == 0)
+		return (0);
+	ret = write(1, s, count);
+	if (ret == -1)
 		return (-1);
-	return (count);
+	return ((int)ret);
 }
