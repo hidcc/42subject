21df3c83002533218950b02583a30f3a0941b87a
hakuta <hidenoriaku@gmail.com>
2026-05-11 10:01:51 +0900

    push

--- python_module1/ex5/ft_plant_types.py
+++ python_module1/ex5/ft_plant_types.py
@@ -4,5 +4,147 @@ class Plant:
         name: str,
         height: float,
         age: int,
-        growth_rate: float
+        growth_rate: float = 0.8,
     ) -> None:
+        self.name = name
+        self.growth_rate = growth_rate
+        if height < 0:
+            print(f"{name}: Error, height can't be negative")
+            self._height = 0.0
+        else:
+            self._height = float(height)
+        if age < 0:
+            print(f"{name}: Error, age can't be negative")
+            self._age = 0
+        else:
+            self._age = age
+
+    def show(self) -> None:
+        print(f"{self.name}: {self._height}cm, {self._age} days old")
+
+    def get_height(self) -> float:
+        return self._height
+
+    def get_age(self) -> int:
+        return self._age
+
+    def set_height(self, height: float) -> None:
+        if height < 0:
+            print(f"{self.name}: Error, height can't be negative")
+            return
+        self._height = float(height)
+
+    def set_age(self, age: int) -> None:
+        if age < 0:
+            print(f"{self.name}: Error, age can't be negative")
+            return
+        self._age = age
+
+    def grow(self) -> None:
+        self._height = round(self._height + self.growth_rate, 2)
+
+    def age(self) -> None:
+        self._age += 1
+
+
+class Flower(Plant):
+    def __init__(
+        self,
+        name: str,
+        height: float,
+        age: int,
+        color: str,
+        growth_rate: float = 0.8,
+    ) -> None:
+        super().__init__(name, height, age, growth_rate)
+        self.color = color
+        self._bloomed = False
+
+    def bloom(self) -> None:
+        self._bloomed = True
+
+    def show(self) -> None:
+        super().show()
+        print(f" Color: {self.color}")
+        if self._bloomed:
+            print(f" {self.name} is blooming beautiully!")
+        else:
+            print(f" {self.name} has not bloomed yet")
+
+
+class Tree(Plant):
+    def __init__(
+        self,
+        name: str,
+        height: float,
+        age: int,
+        trunk_diameter: float,
+        growth_rate: float = 0.8,
+    ) -> None:
+        super().__init__(name, height, age, growth_rate)
+        self.trunk_diameter = float(trunk_diameter)
+
+    def produce_shade(self) -> None:
+        print(
+            f"Tree {self.name} now produces a shade of "
+            f"{self._height}cm long and {self.trunk_diameter}cm wide."
+        )
+
+    def show(self) -> None:
+        super().show()
+        print(f" Trunk diameter: {self.trunk_diameter}cm")
+
+
+class Vegetable(Plant):
+    def __init__(
+        self,
+        name: str,
+        height: float,
+        age: int,
+        harvest_season: str,
+        growth_rate: float = 2.1
+    ) -> None:
+        super().__init__(name, height, age, growth_rate)
+        self.harvest_season = harvest_season
+        self._nutritional_value = 0.0
+
+    def grow(self) -> None:
+        super().grow()
+        self._nutritional_value += 0.5
+
+    def age(self) -> None:
+        super().age()
+        self._nutritional_value += 0.5
+
+    def show(self) -> None:
+        super().show()
+        print(f" Harvest season: {self.harvest_season}")
+        print(f" Nutritional value: {int(self._nutritional_value)}")
+
+
+if __name__ == "__main__":
+    print("=== Garden Plant Types ===")
+
+    print("=== Flower")
+    rose = Flower("Rose", 15.0, 10, "red")
+    rose.show()
+    print("[asking the rose to bloom]")
+    rose.bloom()
+    rose.show()
+    print()
+
+    print("=== Tree")
+    oak = Tree("Oak", 200.0, 365, 5.0)
+    oak.show()
+    print("[asking the oak to produce shade]")
+    oak.produce_shade()
+    print()
+
+    print("=== Vegetable")
+    tomato = Vegetable("Tomato", 5.0, 10, "April")
+    tomato.show()
+    print("[make tomato grow and age for 20 days]")
+    for _ in range(20):
+        tomato.grow()
+        tomato.age()
+    tomato.show()
