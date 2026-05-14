0b34a2ef57e1920a41b1bdd6aa5ca8546555887f
hakuta <hidenoriaku@gmail.com>
2026-05-14 15:07:57 +0900

    fix

---
+++ python_module3/ex5/ft_data_stream.py
@@ -0,0 +1,40 @@
+import random
+import typing
+
+PLAYERS = ("alice", "bob", "charlie", "dylan")
+ACTIONS = ("run", "eat", "sleep", "grab",
+           "move", "climb", "swim", "use", "release")
+
+
+def gen_event() -> typing.Generator[tuple[str, str], None, None]:
+    while True:
+        name = random.choice(PLAYERS)
+        action = random.choice(ACTIONS)
+        yield (name, action)
+
+
+def consume_event(
+    lst: list[tuple[str, str]]
+) -> typing.Generator[tuple[str, str], None, None]:
+    while len(lst) > 0:
+        idx = random.randrange(len(lst))
+        event = lst[idx]
+        del lst[idx]
+        yield event
+
+
+def main() -> None:
+    print("=== Game Data Stream Processor ===")
+    g = gen_event()
+    for i in range(1000):
+        name, action = next(g)
+        print(f"Event {i}: Player {name} did action {action}")
+        events = [next(g) for _ in range(10)]
+    print(f"Built list of 10 events: {events}")
+    for ev in consume_event(events):
+        print(f"Got event from list: {ev}")
+        print(f"Remains in list: {events}")
+
+
+if __name__ == "__main__":
+    main()
