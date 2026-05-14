5f3f051462c69e87110499cf9ae9f96f2550b8fb
hidenori <hidenoriaku@gmail.com>
2026-05-14 14:53:43 +0900

    fix

---
+++ python_module3/ex6/ft_data_alchemist.py
@@ -0,0 +1,10 @@
+import random
+
+
+
+def main() -> None:
+    initial_list = ['Alice', 'bob', 'Charlie', 'dylan', 'Emma', 'Gregory', 'john', 'kevin', 'Liam']
+    capitalized = [player.capitalize() for player in initial_list]
+
+if __name__ == "__main__":
+    main()
