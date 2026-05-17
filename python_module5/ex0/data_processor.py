e0c3f4b9771dd37e9932b16ccec1b982bafca628
hakuta <hidenoriaku@gmail.com>
2026-05-17 17:15:36 +0900

    fix

--- python_module5/ex0/data_processor.py
+++ python_module5/ex0/data_processor.py
@@ -17,7 +17,7 @@ class DataProcessor(ABC):
 
     def output(self) -> tuple[int, str]:
         if not self._storage:
-            raise IndexError("No dat to output")
+            raise IndexError("No data to output")
         return self._storage.pop(0)
 
 
@@ -35,16 +35,16 @@ class NumericProcessor(DataProcessor):
         return False
 
     def ingest(self, data: int | float | list[int | float]) -> None:
-        if not self.validata(data):
+        if not self.validate(data):
             raise ValueError("Improper numeric data")
-        items = data if isinstance(data, list) else [data]:
+        items = data if isinstance(data, list) else [data]
         for item in items:
             self._storage.append((self._counter, str(item)))
             self._counter += 1
 
 
 class TextProcessor(DataProcessor):
-    def validata(self, data: Any) -> bool:
+    def validate(self, data: Any) -> bool:
         if isinstance(data, str):
             return True
         if isinstance(data, list):
@@ -52,10 +52,11 @@ class TextProcessor(DataProcessor):
         return False
 
     def ingest(self, data: str | list[str]) -> None:
-        if not self.validata(data):
+        if not self.validate(data):
             raise ValueError("Improper text data")
+        items = data if isinstance(data, list) else [data]
         for item in items:
-            self._storage.append(self._counter, item)
+            self._storage.append((self._counter, item))
             self._counter += 1
 
 
@@ -70,7 +71,7 @@ class LogProcessor(DataProcessor):
             return all(
                 isinstance(d, dict)
                 and all(
-                    instance(k, str) and isinstance(v, str)
+                    isinstance(k, str) and isinstance(v, str)
                     for k, v in d.items()
                 )
                 for d in data
@@ -79,12 +80,60 @@ class LogProcessor(DataProcessor):
 
     def ingest(
         self, data: dict[str, str] | list[dict[str, str]]
-    )
+    ) -> None:
+        if not self.validate(data):
+            raise ValueError("Improper log data")
+        items = data if isinstance(data, list) else [data]
+        for item in items:
+            text = f"{item['log_level']}: {item['log_message']}"
+            self._storage.append((self._counter, text))
+            self._counter += 1
 
 
 def main() -> None:
     print("=== Code Nexus - Data Processor ===")
     print()
+    print("Testing Numeric Processor...")
+    num = NumericProcessor()
+    print(f" Trying to validate input '42': {num.validate(42)}")
+    print(f" Trying to validate input 'Hello': {num.validate('Hello')}")
+    print(" Test invalid ingestion of string 'foo' without prior validation:")
+    try:
+        num.ingest("foo")
+    except ValueError as e:
+        print(f" Got exception: {e}")
+
+    data_num: list[int | float] = [1, 2, 3, 4, 5]
+    print(f" Processing data: {data_num}")
+    num.ingest(data_num)
+    print(" Extracting 3 values...")
+    for _ in range(3):
+        key, value = num.output()
+        print(f" Numeric value {key}: {value}")
+    print()
+
+    print("Testing Text Processor...")
+    text = TextProcessor()
+    print(f" Trying to validate input '42': {text.validate(42)}")
+    data_text = ["Hello", "Nexus", "World"]
+    print(f" Processing data: {data_text}")
+    text.ingest(data_text)
+    print(" Extracting 1 value...")
+    key, value = text.output()
+    print(f" Text value {key}: {value}")
+    print()
+
+    print("Testing Log Processor...")
+    log = LogProcessor()
+    print(f" Trying to validate input 'Hello': {log.validate('Hello')}")
+    data_log = [{'log_level': 'NOTICE', 'log_message': 'Connection to server'}, {
+        'log_level': 'ERROR', 'log_message': 'Unauthorized access!!'}]
+    print(f" Processing data: {data_log}")
+    log.ingest(data_log)
+    print(" Extracting 2 values...")
+    for _ in range(2):
+        key, value = log.output()
+        print(f" Log entry {key}: {value}")
 
 
 if __name__ == "__main__":
