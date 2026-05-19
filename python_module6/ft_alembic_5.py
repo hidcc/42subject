cc2e97c25412c83136148ecd4eafc0adf79457af
hidenori <hidenoriaku@gmail.com>
2026-05-19 14:55:45 +0900

    fix

--- python_module6/ft_alembic_5.py
+++ python_module6/ft_alembic_5.py
@@ -1,4 +1,5 @@
-from alchemy import 
+from alchemy import create_air
 
+print("=== Alembic 5 ===")
 print("Accessing the alchemy module using 'from alchemy import ...'")
 print(f"Testing create_air: {air}")
