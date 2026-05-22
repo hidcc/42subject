244d149a0e3d3c92bbe56a1a13a5c2771a942fd1
hakuta <hidenoriaku@gmail.com>
2026-05-22 14:26:11 +0900

    fix

--- python_module6/alchemy/potions.py
+++ python_module6/alchemy/potions.py
@@ -3,8 +3,14 @@ from elements import create_fire, create_water
 
 
 def healing_potion() -> str:
-    return (f"Healing potion brewed with '{create_earth()}' and '{create_air()}'")
+    return (
+        f"Healing potion brewed with "
+        f"'{create_earth()}' and '{create_air()}'"
+    )
 
 
 def strength_potion() -> str:
-    return (f"Strength potion brewed with '{create_fire()}' and '{create_water()}'")
+    return (
+        f"Strength potion brewed with "
+        f"'{create_fire()}' and '{create_water()}'"
+    )
