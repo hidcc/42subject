c3275a5871a2f8dfa316df89d1323669191b9cf9
hakuta <hidenoriaku@gmail.com>
2026-06-07 10:54:50 +0900

    ex0

---
+++ python_module10/ex4/decorator_master.py
@@ -0,0 +1,13 @@
+def artifact_sorter(artifacts: list[dict]) -> list[dict]:
+    artifacts.sort(key=lambda x: x['power'])
+
+
+def power_filter(mages: list[dict], min_power: int) -> list[dict]
+def spell_transformer(spells: list[str]) -> list[str]
+def mage_stats(mages: list[dict]) -> dict
+
+
+def main():
+
+
+__name__ == "__main__"
